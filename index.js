e6098bca6413bf649652dac0967ea896a924ba1f
Audrey Tang <audreyt@audreyt.org>
2013-04-24 14:11:09 +0800

    * Stringify the arguments, and experiment with callconv

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 (function(){
-  var unorm, out$ = typeof exports != 'undefined' && exports || this;
+  var unorm;
   unorm = require('unorm');
-  out$.toFullwidth = toFullwidth;
   function toFullwidth(it){
-    return unorm.nfd(it).replace(/ /g, '\u3000').replace(/[\u0020-\u007E]/g, function(it){
+    return unorm.nfd(it + "").replace(/ /g, '\u3000').replace(/[\u0020-\u007E]/g, function(it){
       return String.fromCharCode(it.charCodeAt(0) + 0xFEE0);
     });
   }
-  out$.toFullwidthNFC = toFullwidthNFC;
   function toFullwidthNFC(it){
     return unorm.nfc(toFullwidth(it));
   }
-  out$.toHalfwidth = toHalfwidth;
   function toHalfwidth(x){
-    return unorm.nfd(it).replace(/\u3000/g, ' ').replace(/[\uFF01-\uFF5E]/g, function(it){
+    return unorm.nfd(it + "").replace(/\u3000/g, ' ').replace(/[\uFF01-\uFF5E]/g, function(it){
       return String.fromCharCode(it.charCodeAt(0) - 0xFEE0);
     });
   }
-  out$.toHalfwidthNFC = toHalfwidthNFC;
   function toHalfwidthNFC(it){
     return unorm.nfc(toHalfwidth(it));
   }
+  module.exports = toFullwidth;
+  module.exports.toFullwidth = toFullwidthNFC;
+  module.exports.toHalfwidth = toHalfwidthNFC;
+  module.exports.toFullwidthNFC = toFullwidthNFC;
+  module.exports.toHalfwidthNFC = toFullwidthNFC;
 }).call(this);
